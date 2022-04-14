56e5fa6f9b9b533f7ee9cff0b903fef1a654909e
chyroc <chyroc@qq.com>
2022-04-14 22:20:09 +0800

    fix exec dep

--- src/run.ts
+++ src/run.ts
@@ -1,11 +1,37 @@
-import process from 'process'
-import execa from 'execa'
+const { spawn } = require('child_process')
+
+export async function runCommand(command: string, args: string[]): Promise<{ code: number; stdout: string; stderr: string }> {
+  return new Promise((resolve, reject) => {
+    const ls = spawn(command, args)
+
+    let stdout = ''
+    let stderr = ''
+
+    ls.stdout.on('data', (data: Buffer) => {
+      stdout = data.toString()
+    })
+
+    ls.stderr.on('data', (data: Buffer) => {
+      stderr = data.toString()
+    })
+
+    ls.on('close', (code: number) => {
+      resolve({ stdout, stderr, code })
+    })
+
+    ls.on('error', (err: Error) => {
+      reject(err)
+    })
+  })
+}
 
 export async function runAppleScript(script: any) {
   if (process.platform !== 'darwin') {
     throw new Error('macOS only')
   }
-
-  const { stdout } = await execa('osascript', ['-e', script])
-  return stdout
+  const { stdout, stderr, code } = await runCommand('osascript', ['-e', script])
+  if (stdout) {
+    return stdout
+  }
+  throw new Error(stderr)
 }
